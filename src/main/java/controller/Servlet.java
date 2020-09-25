d43ddcd598c60d3ab179eec44234299c0fec4475
Nghia <vunghia2000@gmail.com>
2020-09-25 10:40:27 +0700

    nghia

--- src/main/java/controller/Servlet.java
+++ src/main/java/controller/Servlet.java
@@ -4,17 +4,13 @@ import dao.FakeData;
 import dao.WatchDAO;
 import entities.Admin;
 import entities.Watch;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @javax.servlet.annotation.WebServlet("/watches")
@@ -106,6 +102,5 @@ public class Servlet extends HttpServlet {
         } catch (ServletException | IOException e) {
             e.printStackTrace();
         }
-
     }
 }
